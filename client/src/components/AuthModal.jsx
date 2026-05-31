afd7f1f63915e788afc2f3c5dcf267255d69afb5
DakshPrajapati81 <daksh0583@gmail.com>
2026-05-31 11:03:14 +0530

    fourth commit

--- client/src/components/AuthModal.jsx
+++ client/src/components/AuthModal.jsx
@@ -6,9 +6,7 @@ import { FcGoogle } from 'react-icons/fc';
 
 const AuthModal = ({ isOpen, onClose }) => {
   const { login } = useAuth();
-  setTimeout(() => {
-    window.location.href = `${import.meta.env.VITE_BACKEND_URL}/api/auth`;
-  }, 1000);
+  
   const googleLogin = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
       try {
